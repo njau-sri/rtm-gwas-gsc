7bd7b30ea16107a5146186f300c96b7521fdc6af
Jianbo He <hjbxyz@gmail.com>
2018-10-31 15:41:10 +0800

    Use netlib blas/lapack

--- install-centos.sh
+++ install-centos.sh
@@ -5,14 +5,14 @@ mkdir $1
 
 if [ $1 == "lnx64" ]; then
 
-    g++ *.cpp -o $1/rtm-gwas-gsc -s -O2 -std=c++11 -static -lopenblas -lgfortran -lquadmath -fopenmp
+    g++ *.cpp -o $1/rtm-gwas-gsc -s -O2 -std=c++11 -static -llapack -lblas -lgfortran -lquadmath -fopenmp
 
 elif [ $1 == "win32" ]; then
 
-    i686-w64-mingw32-g++ *.cpp -o $1/rtm-gwas-gsc.exe -s -O2 -std=c++11 -static -lopenblas -lgfortran -lquadmath -fopenmp
+    i686-w64-mingw32-g++ *.cpp -o $1/rtm-gwas-gsc.exe -s -O2 -std=c++11 -static -llapack -lrefblas -lgfortran -lquadmath -fopenmp
 
 elif [ $1 == "win64" ]; then
 
-    x86_64-w64-mingw32-g++ *.cpp -o $1/rtm-gwas-gsc.exe -s -O2 -std=c++11 -static -lopenblas -lgfortran -lquadmath -fopenmp
+    x86_64-w64-mingw32-g++ *.cpp -o $1/rtm-gwas-gsc.exe -s -O2 -std=c++11 -static -llapack -lrefblas -lgfortran -lquadmath -fopenmp
 
 fi
--- install-macos.sh
+++ install-macos.sh
@@ -8,4 +8,4 @@ LIBGFORTRAN=${LIB}/libgfortran.a
 LIBQUADMATH=${LIB}/libquadmath.a
 LIBGCC=${LIB}/gcc/x86_64-apple-darwin17.5.0/8.1.0/libgcc.a
 
-g++ *.cpp -o macos/rtm-gwas-gsc -O2 -std=c++11 -lopenblas $LIBGFORTRAN $LIBQUADMATH $LIBGCC
+g++ *.cpp -o macos/rtm-gwas-gsc -O2 -std=c++11 -llapack -lrefblas $LIBGFORTRAN $LIBQUADMATH $LIBGCC
