224fdd4727a0d50a24ec8d3e3b7b12744181f6fd
Jianbo He <hjbxyz@gmail.com>
2018-11-04 14:01:32 +0800

    Update install

--- install-macos.sh
+++ install-macos.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-rm -rf macos
-mkdir macos
-
 LIB=/usr/local/lib
+
 LIBGFORTRAN=${LIB}/libgfortran.a
 LIBQUADMATH=${LIB}/libquadmath.a
 LIBGCC=${LIB}/gcc/x86_64-apple-darwin17.5.0/8.1.0/libgcc.a
+
+rm -rf macos
+mkdir macos
 
 g++ *.cpp -o macos/rtm-gwas-gsc -O2 -std=c++11 -llapack -lrefblas $LIBGFORTRAN $LIBQUADMATH $LIBGCC
