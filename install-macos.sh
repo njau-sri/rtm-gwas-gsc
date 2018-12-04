d90cfe606d0fd429db736b4ce57f21666b477352
Jianbo He <hjbxyz@gmail.com>
2018-12-04 13:26:32 +0800

    Get version from environment variable

--- install-macos.sh
+++ install-macos.sh
@@ -9,4 +9,11 @@ LIBGCC=${LIB}/gcc/x86_64-apple-darwin17.5.0/8.1.0/libgcc.a
 rm -rf macos
 mkdir macos
 
-g++ *.cpp -o macos/rtm-gwas-gsc -O2 -std=c++11 -llapack -lrefblas $LIBGFORTRAN $LIBQUADMATH $LIBGCC
+TARGET=macos/rtm-gwas-gsc
+
+if [ -z "$RTM_GWAS_VERSION" ]; then
+    RTM_GWAS_VERSION=unknown
+fi
+
+g++ *.cpp -o $TARGET -O2 -std=c++11 -llapack -lrefblas $LIBGFORTRAN $LIBQUADMATH $LIBGCC \
+    -DRTM_GWAS_VERSION=$RTM_GWAS_VERSION
